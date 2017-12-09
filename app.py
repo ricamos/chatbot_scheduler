6e001cba2ae6e507bbdfa0a5fdd4ec741624090d
Ricardo Coelho <ricardo.rcj@gmail.com>
2017-12-08 23:06:48 -0200

    Fix:  Verification Token does not work
    - final repair
    - error tiping

--- app.py
+++ app.py
@@ -17,13 +17,12 @@ https://wiki.python.org/moin/PythonDecorators
 '''
 @app.route('/', methods=['GET'])
 def verify():
-
     # Webhook verification
-    # if request.args.get("hub.mod") == "subscribe" and request.args.get("hub.challenge"):
-    if not request.args.get("hub.verify_token") == "hello":
-        return "Verification token mismatch", 403
-    return request.args["hub.challenge"], 200
-    # return "Hello world", 200
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
 
 @app.route('/', methods=['POST'])
 def webhook():
